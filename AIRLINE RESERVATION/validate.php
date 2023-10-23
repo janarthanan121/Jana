a2ac5b73dfaafba1727d6df952622f91eeafc83f
janarthanan121 <113823867+janarthanan121@users.noreply.github.com>
2023-10-23 10:44:56 +0530

    Add files via upload

---
+++ AIRLINE RESERVATION/validate.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Validation Page</title>
+</head>
+<body>
+<?php
+ echo $_POST["name"];
+ echo $_POST["password"];
+ ?>
+ <?php
+ echo $_POST['name'];
+ echo $_POST["email"];
+ echo $_POST["password"];
+ 
+ ?>
+</body>
+</html>
